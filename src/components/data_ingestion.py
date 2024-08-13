1b4bdd5e5008197f76fbd54bf4e32b5db0fdd361
ajasan2 <105524167+ajasan2@users.noreply.github.com>
2024-08-13 18:45:16 -0500

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,17 +1,22 @@
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.exception import CustomException
+from src.logger import logging
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
     test_data_path: str = os.path.join('artifacts', 'test.csv')
     raw_data_path: str = os.path.join('artifacts', 'data.csv')
+
 
 class DataIngestion:
     def __init__(self):
@@ -41,5 +46,8 @@ class DataIngestion:
 
 
 if __name__ == '__main__':
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    data_ingestion = DataIngestion()
+    train_data, test_data = data_ingestion.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
